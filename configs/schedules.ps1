7f81065edd2c806b0177a3b2dfd3c68b9d119e44
rbleattler <40604784+rbleattler@users.noreply.github.com>
2021-02-12 17:57:55 -0500

    Update schedules.ps1

--- configs/schedules.ps1
+++ configs/schedules.ps1
@@ -1,2 +1,2 @@
-New-UASchedule -Script "CheckForUniversalUpdates.ps1" -Credential 'UA.Credential.PSSrv' -Cron '0 5 0 * * ?' -Environment '5.1.17763.1490'
-New-UASchedule -Script "GetLatestUniversalRelease.ps1" -Credential 'UA.Credential.PSSrv' -Cron '0 0 0 * * ?'
+New-UASchedule -Script "CheckForUniversalUpdates.ps1" -Cron '0 5 0 * * ?' -Environment '5.1.17763.1490'
+New-UASchedule -Script "GetLatestUniversalRelease.ps1" -Cron '0 0 0 * * ?'
